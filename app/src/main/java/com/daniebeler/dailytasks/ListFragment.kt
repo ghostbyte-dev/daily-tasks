3020495bdf61a6833fe2eec1f48c5f704b39f16c
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-01-30 23:30:31 +0100

    added empty state

--- app/src/main/java/com/daniebeler/dailytasks/ListFragment.kt
+++ app/src/main/java/com/daniebeler/dailytasks/ListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 class ListFragment : Fragment(), TodoAdapter.OnItemClickListener, TodoAdapter.OnItemLongclickListener {
 
     private var recyclerView: RecyclerView? = null
+    private lateinit var textView: TextView
     private var day = ""
     private lateinit var mainActivity: MainActivity
 
@@ -32,6 +34,10 @@ class ListFragment : Fragment(), TodoAdapter.OnItemClickListener, TodoAdapter.On
         recyclerView = requireView().findViewById(R.id.rv_dashboard_today)
         recyclerView?.layoutManager = LinearLayoutManager(activity?.applicationContext)
 
+        textView = requireView().findViewById(R.id.tv_empty)
+
+        textView.visibility = View.GONE
+
         Log.d("state", "Listfragment: onViewCreated id:" + this)
 
         refreshList()
@@ -39,6 +45,14 @@ class ListFragment : Fragment(), TodoAdapter.OnItemClickListener, TodoAdapter.On
 
     fun refreshList(){
         recyclerView?.adapter = TodoAdapter(mainActivity.dbHandler.getToDos(day), this, this)
+
+        Log.d("loog", (recyclerView?.adapter)?.itemCount.toString())
+
+        if ((recyclerView?.adapter)?.itemCount == 0) {
+            textView.visibility = View.VISIBLE
+        } else {
+            textView.visibility = View.INVISIBLE
+        }
     }
 
     override fun onItemClick(position: Int) {

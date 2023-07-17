a46cac063ed3e81597018093f12aafa707a817e4
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-07-17 14:37:47 +0200

    Migrated to Jetpack Compose

--- app/src/main/java/com/daniebeler/dailytasks/MainActivity.kt
+++ app/src/main/java/com/daniebeler/dailytasks/MainActivity.kt
@@ -7,14 +7,24 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     private lateinit var btnAdd: Button
     private lateinit var btnIvy: Button
@@ -22,9 +32,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomSheetDialog: BottomSheetDialogFragment
 
     private lateinit var viewPager: ViewPager2
-
-    private lateinit var todayFragment: ListFragment
-    private lateinit var tomorrowFragment: ListFragment
 
     lateinit var dbHandler: DBHandler
 
@@ -34,18 +41,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setContent {
+
+            var tabIndex by remember { mutableStateOf(0) }
+            TabRow(selectedTabIndex = 0) {
+                Tab(selected = tabIndex == 0, onClick = { /*TODO*/ }) {
+                    Text ("soos")
+                }
+            }
+        }
+
         themeSharedPreferences =  getSharedPreferences("DAILY_TASKS", Context.MODE_PRIVATE)
         if (themeSharedPreferences.getString("theme", "light") != "light") {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
 
-        initViewPager2()
+        //initViewPager2()
 
         dbHandler = DBHandler(this)
 
-        btnAdd = findViewById(R.id.btn_add)
-        btnAdd.setOnClickListener{
-            bottomSheetDialog = BottomSheetInput()
+//        btnAdd = findViewById(R.id.btn_add)
+//        btnAdd.setOnClickListener{
+            /*bottomSheetDialog = BottomSheetInput()
             val bundle = Bundle()
             if(viewPager.currentItem == 0){
                 bundle.putString("date", "today")
@@ -54,16 +71,16 @@ class MainActivity : AppCompatActivity() {
                 bundle.putString("date", "tomorrow")
             }
 
-            bottomSheetDialog.arguments = bundle
-            bottomSheetDialog.show(supportFragmentManager, "tag")
+            bottomSheetDialog.arguments = bundle*/
+            //bottomSheetDialog.show(supportFragmentManager, "tag")
         }
 
-        btnIvy = findViewById(R.id.btn_ivy)
+       /* btnIvy = findViewById(R.id.btn_ivy)
         btnIvy.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://jamesclear.com/ivy-lee")))
-        }
+        }*/
 
-        btnMode = findViewById(R.id.btn_mode)
+       /* btnMode = findViewById(R.id.btn_mode)
         btnMode.setOnClickListener {
             if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
                 themeSharedPreferences.edit().putString("theme", "dark").apply()
@@ -72,10 +89,10 @@ class MainActivity : AppCompatActivity() {
                 themeSharedPreferences.edit().putString("theme", "light").apply()
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-        }
+        }*/
     }
 
-    private fun initViewPager2(){
+/*    private fun initViewPager2(){
         viewPager = findViewById(R.id.viewpager)
         val adapter = StateAdapter(this)
         viewPager.adapter = adapter
@@ -90,15 +107,5 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(tabLayout, viewPager){tab,position ->
             tab.text = names[position]
         }.attach()
-    }
+    }*/
 
-    fun refresh(date:String){
-        Log.d("state", "Main: refreshing")
-        if(date == "today"){
-            todayFragment.refreshList()
-        }
-        else{
-            tomorrowFragment.refreshList()
-        }
-    }
-}
